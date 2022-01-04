65d499ac31ec9aa929815677346e3ddceb0b4952
Matheus-Msantos <matheusmelosantos10@gmail.com>
2022-01-04 18:09:25 -0300

    Criando html de listagem de tarefas

--- my-app/src/Listar/Listar-tarefas.js
+++ my-app/src/Listar/Listar-tarefas.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function ListarTarefas() {
-    return (
-        <div className="g-listar-container">
-            <a href="/cadastrar">Nova tarefa</a>
+
+  const [tarefas, setTarefas] = useState([]);
+  const [carregarTarefas, setCarregarTarefas] = useState(true);
+
+  useEffect(() => {
+
+    const obterTarefas = () => {
+      const tarefasDb = localStorage['tarefas'];
+      let listarTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+      setTarefas(listarTarefas)
+      console.log(listarTarefas);
+    }
+
+    if (carregarTarefas) {
+      obterTarefas();
+      setCarregarTarefas(false)
+    }
+
+  }, [carregarTarefas]);
+
+  return (
+    <div className="g-listar-container">
+      <h1>Lista de Tarefas</h1>
+
+      <div className="g-listar-table">
+        <div className="g-listar-table-head">
+          <p>Tarefas</p>
+          <a href="/cadastrar">Nova tarefa</a>
         </div>
-    );
+
+        <div className="g-listar-body">
+
+        </div>
+      </div>
+
+    </div>
+  );
 }
 
 export default ListarTarefas
